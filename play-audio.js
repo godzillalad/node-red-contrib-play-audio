b2c0d43ce4c260b8854167ff6e89d052f1c5d8a3
Shane <godzillalad@gmail.com>
2016-10-07 22:34:18 +0100

    renaming and adding latest play js

--- play-audio.js
+++ play-audio.js
@@ -3,6 +3,7 @@ module.exports = function(RED) {
 		RED.nodes.createNode(this,config);
 		var node = this;
 		this.on('input', function(msg) {
+			console.log("publish playaudio msg");
 			RED.comms.publish("playaudio", msg.payload);
 		});
 	}
